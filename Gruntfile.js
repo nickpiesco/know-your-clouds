f2e30130ae68eec4b8c02ce5e96c4353ae1c134e
Nick Piesco <nickpiesco@gmail.com>
2014-03-01 16:19:14 -0600

    Removed comments from Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,5 @@
 module.exports = function(grunt) {
 
-	// 1. All configuration goes here 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		watch: {
@@ -24,11 +23,9 @@ module.exports = function(grunt) {
 		}
 	});
 
-	// 3. Where we tell Grunt we plan to use this plug-in.
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 
-	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
 	grunt.registerTask('default', ['watch', 'sass']);
 
 };
