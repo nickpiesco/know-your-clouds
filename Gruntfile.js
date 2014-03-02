bcca5427481e9d56a67103ef31806b8dbcddfba8
Nick Piesco <nickpiesco@gmail.com>
2014-03-02 17:59:32 -0600

    Squished images and JS, updated README.md

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,21 +11,52 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+		concat: {
+			dist: {
+				src: [
+					'src/js/prototype.js',
+					'src/js/scriptaculous.js',
+					'src/js/effects.js',
+					'src/js/builder.js',
+					'src/js/lightbox.js'
+				],
+				dest: 'dist/js/all.js'
+			}
+		},
+		uglify: {
+			build: {
+				src: 'dist/js/all.js',
+				dest: 'dist/js/all.min.js'
+			}
+		},
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: 'src/images/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'dist/images/'
+				}]
+			}
+		},
 		sass: {
 			dist: {
 				options: {
 					style: 'compressed'
 				},
 				files: {
-					'src/css/style.css': 'src/scss/style.scss'
+					'dist/css/style.css': 'src/scss/style.scss'
 				}
 			} 
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 
-	grunt.registerTask('default', ['watch', 'sass']);
+	grunt.registerTask('default', ['watch', 'concat', 'uglify', 'imagemin', 'sass']);
 
 };
